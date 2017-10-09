bfcc92642283be689211e204e92737cb1d62a841
raghav <raghav.chalapathy@gmail.com>
2017-10-09 16:23:20 +1100

    latest

--- nn_models/tflearn_OneClassClassifier.py
+++ nn_models/tflearn_OneClassClassifier.py
@@ -43,7 +43,7 @@ print "No_of_hiddenNodes",No_of_hiddenNodes
 
 
 input_layer = input_data(shape=[None, No_of_inputNodes]) #input layer of size 2
-hidden_layer = fully_connected(input_layer , No_of_hiddenNodes, activation='tanh',name="hiddenLayer_Weights") #hidden layer of size 2
+hidden_layer = fully_connected(input_layer , No_of_hiddenNodes, activation='tanh',name="hiddenLayer_Weights",weights_init="normal") #hidden layer of size 2
 output_layer = fully_connected(hidden_layer, 1, activation='sigmoid',name="outputLayer_Weights") #output layer of size 1
 
 # Hyper parameters for the one class Neural Network
