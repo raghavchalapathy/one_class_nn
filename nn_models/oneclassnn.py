1ce98dc00e8f36791ab5e350528a9a258494b4ad
raghav <raghav.chalapathy@gmail.com>
2017-10-12 22:57:20 +1100

    changed the initialzation values of weights

--- nn_models/oneclassnn.py
+++ nn_models/oneclassnn.py
@@ -31,10 +31,7 @@ X = data_train
 Y = targets_train
 Y = Y.tolist()
 Y = [[i] for i in Y]
-data_train = data[0:220]
-targets_train = target[0:220]
-data_test = data[220:231]
-targets_test = target[220:231]
+
 # For testing the algorithm
 X_test = data_test
 Y_test = targets_test
@@ -44,17 +41,22 @@ Y_test = [[i] for i in Y_test]
 No_of_inputNodes = X.shape[1]
 K = 4
 nu = 0.04
+D = X.shape[1]
 
 # Define the network
 input_layer = input_data(shape=[None, No_of_inputNodes])  # input layer of size
+
+np.random.seed(42)
+theta0 = np.random.normal(0, 1, K + K*D + 1) *0.0001
+#theta0 = np.random.normal(0, 1, K + K*D + 1) # For linear
 hidden_layer = fully_connected(input_layer, 4, bias=False, activation='sigmoid', name="hiddenLayer_Weights",
                                weights_init="normal")  # hidden layer of size 2
+
+
 output_layer = fully_connected(hidden_layer, 1, bias=False, activation='linear', name="outputLayer_Weights",
                                weights_init="normal")  # output layer of size 1
 
-# assign the learnt weights
-wStar = hidden_layer.W
-VStar = output_layer.W
+
 
 # Hyper parameters for the one class Neural Network
 v = 0.04
@@ -64,6 +66,7 @@ nu = 0.04
 value = 0.01
 init = tf.constant_initializer(value)
 rho = va.variable(name='rho', dtype=tf.float32, shape=[], initializer=init)
+
 rcomputed = []
 auc = []
 
@@ -74,33 +77,51 @@ print sess.run(tflearn.get_training_mode()) #False
 tflearn.is_training(True, session=sess)
 print sess.run(tflearn.get_training_mode())  #now True
 
-oneClassNN = oneClassNN(output_layer, v, rho, hidden_layer, output_layer, optimizer='sgd', loss='OneClassNN_Loss',
-                            learning_rate=5)
+X  = data_train
+D  = X.shape[1]
+nu = 0.04
+
+
+
+
+# temp = np.random.normal(0, 1, K + K*D + 1)[-1]
+
+temp = theta0[-1]
+
+# temp = tflearn.variables.get_value(rho, session=sess)
+
+oneClassNN = oneClassNN(output_layer, v, rho, hidden_layer, output_layer, optimizer='sgd',
+                        loss='OneClassNN_Loss',
+                            learning_rate=1)
 
 model = DNN(oneClassNN, tensorboard_verbose=3)
+
+model.set_weights(output_layer.W, theta0[0:K][:,np.newaxis])
+model.set_weights(hidden_layer.W, np.reshape(theta0[K:K +K*D],(D,K)))
+
 
 iterStep = 0
 while (iterStep < 100):
     print "Running Iteration :", iterStep
     # Call the cost function
-
-
     y_pred = model.predict(data_train)  # Apply some ops
+    tflearn.is_training(False, session=sess)
     y_pred_test = model.predict(data_test)  # Apply some ops
+    tflearn.is_training(True, session=sess)
     value = np.percentile(y_pred, v * 100)
     tflearn.variables.set_value(rho, value,session=sess)
     rStar = rho
-    model.fit(X, Y, n_epoch=1, show_metric=True, batch_size=220)
+    model.fit(X, Y, n_epoch=2, show_metric=True, batch_size=100)
     iterStep = iterStep + 1
     rcomputed.append(rho)
     temp = tflearn.variables.get_value(rho, session=sess)
 
+    # print "Rho",temp
+    # print "y_pred",y_pred
+    # print "y_predTest", y_pred_test
 
-    print "y_pred",y_pred
-    print "y_predTest", y_pred_test
-
-g = lambda x: x
-
+# g = lambda x: x
+g   = lambda x : 1/(1 + tf.exp(-x))
 
 def nnScore(X, w, V, g):
     return tf.matmul(g((tf.matmul(X, w))), V)
@@ -110,6 +131,15 @@ def nnScore(X, w, V, g):
 X = X.astype(np.float32)
 X_test = data_test
 X_test = X_test.astype(np.float32)
+# assign the learnt weights
+# wStar = hidden_layer.W
+# VStar = output_layer.W
+# Get weights values of fc2
+wStar = model.get_weights(hidden_layer.W)
+VStar = model.get_weights(output_layer.W)
+
+print "Hideen",wStar
+print VStar
 
 train = nnScore(X, wStar, VStar, g)
 test = nnScore(X_test, wStar, VStar, g)
@@ -121,11 +151,11 @@ test = nnScore(X_test, wStar, VStar, g)
 arrayTrain = train.eval(session=sess)
 arrayTest = test.eval(session=sess)
 
-print "Train Array:",arrayTrain
-print "Test Array:",arrayTest
+# print "Train Array:",arrayTrain
+# print "Test Array:",arrayTest
 
-plt.hist(arrayTrain, label='Normal');
-plt.hist(arrayTest, label='Anomalies');
+plt.hist(arrayTrain-temp,  bins = 25,label='Normal');
+plt.hist(arrayTest-temp, bins = 25, label='Anomalies');
 plt.legend(loc='upper right')
-# plt.title('r = %1.6f' % rStar)
+plt.title('r = %1.6f- Sigmoid Activation ' % temp)
 plt.show()
